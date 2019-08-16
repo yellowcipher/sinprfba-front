3260772d7f072599ae8755f05fb287eb9185d3f1
harimasora <zidalo@gmail.com>
2019-08-16 02:21:19 -0300

    fix(css): Remove unresponsive styles

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: [ './home-page.component.scss' ],
 })
 export class HomePageComponent implements OnInit {
-	images = [ 1, 2, 3 ].map(() => `https://picsum.photos/2550/1700?random&t=${Math.random()}`);
+	images = [ 1, 2, 3 ].map(() => `https://picsum.photos/510/340?random&t=${Math.random()}`);
 
 	constructor() {}
 
