b62ea3e3ea0baf2757e7c26b0b6b1ee28a20c6dd
Jedhai423 <jedhai.psn@gmail.com>
2019-09-20 15:51:19 -0300

    feat(institutional): link 'institutional' with firebase

--- src/app/institutional-page/institutional-page.component.ts
+++ src/app/institutional-page/institutional-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardService, Board } from './../services/board.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-institutional-page',
-  templateUrl: './institutional-page.component.html',
-  styleUrls: ['./institutional-page.component.scss']
+	selector: 'app-institutional-page',
+	templateUrl: './institutional-page.component.html',
+	styleUrls: [ './institutional-page.component.scss' ],
 })
 export class InstitutionalPageComponent implements OnInit {
+	board$: Observable<Board[]>;
 
-  constructor() { }
+	constructor(private boardService: BoardService) {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.board$ = this.boardService.list();
+	}
 }
